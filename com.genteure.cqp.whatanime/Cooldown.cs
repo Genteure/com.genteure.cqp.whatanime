162c0e96e3c0d7afa855d69d0b1c40156e55e04d
Genteure <genteure@gmail.com>
2017-11-05 08:21:27 +0800

    update

---
+++ com.genteure.cqp.whatanime/Cooldown.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.genteure.cqp.whatanime
+{
+    internal static class Cooldown
+    {
+        // /api/me 所返回的初始状态
+
+        internal static int InitialQuota = 10;
+        internal static int InitialQuotaTTL = 60;
+
+
+        // /api/search 返回的状态
+
+        internal static int Quota = 10;
+        internal static DateTime Expire = DateTime.Now;
+
+        /// <summary>
+        /// 当前可以执行搜索
+        /// </summary>
+        internal static bool CanSearch => Quota > 0;
+
+        /// <summary>
+        /// 剩余冷却时间
+        /// </summary>
+        internal static double RemainingSeconds => CanSearch ? 0d : (Expire - DateTime.Now).TotalSeconds;
+
+        /// <summary>
+        /// 设置新的 ratelimit 参数
+        /// </summary>
+        /// <param name="quota"></param>
+        /// <param name="expire"></param>
+        internal static void SetCooldown(int quota, int expire)
+        {
+            Quota = quota;
+            Expire = DateTime.Now + TimeSpan.FromSeconds(expire);
+        }
+    }
+}
---
+++ com.genteure.cqp.whatanime/ImageProcessor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace com.genteure.cqp.whatanime
+{
+    internal static class ImageProcessor
+    {
+        private static readonly Regex CQIMG_REGEX = new Regex("^url=(http.*)$", RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        private const int HTTP_TIMEOUT = 10 * 1000;
+        private const string HTTP_USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36";
+
+        /// <summary>
+        /// 从接收到的 CQIMG 文件转换为压缩后的 data/jpeg Base64 文本
+        /// </summary>
+        /// <param name="cqimg">接收到的文件名</param>
+        /// <returns>data:image/jpeg;base64,{0}</returns>
+        internal static async Task<string> CQIMG2Base64Async(string cqimg)
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "image", cqimg + ".cqimg");
+            var url = CQIMG_REGEX.Match(File.ReadAllText(path)).Groups[1].Value;
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Timeout = HTTP_TIMEOUT;
+            request.UserAgent = HTTP_USERAGENT;
+
+            var response = (HttpWebResponse)(await request.GetResponseAsync());
+            MemoryStream result = new MemoryStream();
+            new Bitmap(response.GetResponseStream()).Save(result, GetEncoderInfo("image/jpeg"), new EncoderParameters()
+            {
+                Param = new EncoderParameter[] { new EncoderParameter(Encoder.Quality, 50L) }
+            });
+
+            return Convert.ToBase64String(result.ToArray());
+        }
+
+        /// <summary>
+        /// 下载网络图片到本地并转换成可直接发送的 CQ码
+        /// </summary>
+        /// <param name="url">图片地址</param>
+        /// <returns>CQ码</returns>
+        internal static async Task<string> WebImage2CQIMGAsync(string url)
+        {
+            var dir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "image", "whatanime");
+            if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Timeout = HTTP_TIMEOUT;
+            request.UserAgent = HTTP_USERAGENT;
+
+            var response = (HttpWebResponse)(await request.GetResponseAsync());
+            string filename = ((int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds).ToString("X") + "-" + url.GetHashCode().ToString("X") + ".jpg";
+            // TODO: 图片文件拓展名
+            return CoolQApi.CQC_Image("whatanime\\" + filename);
+
+        }
+
+
+        private static ImageCodecInfo GetEncoderInfo(String mimeType)
+            => ImageCodecInfo.GetImageEncoders().ToList().First(x => x.MimeType == mimeType);
+
+    }
+}
